fe7a7fad75d9a49abf45a6e34ef7aa10c370d415
sachchidatripathi <sntripathi01@gmail.com>
2021-08-04 14:18:47 -0600

    Lambda function updated

--- src/main/java/com/lambda/demo/Hello.java
+++ src/main/java/com/lambda/demo/Hello.java
@@ -3,17 +3,12 @@ package com.lambda.demo;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-import java.util.Map;
-
-public class Hello implements RequestHandler<Map<String, String>, LambdaResponse> {
+public class Hello implements RequestHandler<LambdaRequest, LambdaResponse> {
     @Override
-    public LambdaResponse handleRequest(Map<String, String> requestMap, Context context) {
+    public LambdaResponse handleRequest(LambdaRequest request, Context context) {
         System.out.printf("Request land on lambda function");
-        if (requestMap != null) {
-            requestMap.forEach((s, s2) -> {
-                System.out.printf(" Request key = " + s);
-                System.out.printf(" Request value = " + s2);
-            });
+        if (request != null) {
+            System.out.printf(" Request  = " + request.getBody());
         }
         LambdaResponse response = new LambdaResponse();
         response.setBody("Hello from java Lambda!");
